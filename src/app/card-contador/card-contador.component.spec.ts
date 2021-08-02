6411d37553bae61219e5b91e9f8eb51bdd6fd6ba
girdacio <girdacio@gmail.com>
2021-08-01 22:53:26 -0300

    test(card-contador, card-gerenciador-contador) add store mock spy

--- src/app/card-contador/card-contador.component.spec.ts
+++ src/app/card-contador/card-contador.component.spec.ts
@@ -1,14 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Store } from '@ngrx/store';
 
 import { CardContadorComponent } from './card-contador.component';
 
 describe('CardContadorComponent', () => {
+  const storeSpy = jasmine.createSpyObj('Store', ['select']);
   let component: CardContadorComponent;
   let fixture: ComponentFixture<CardContadorComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CardContadorComponent ]
+      declarations: [ CardContadorComponent ],
+      providers: [{ provide: Store, useValue: storeSpy }]
     })
     .compileComponents();
   });
--- src/app/card-gerenciador-contador/card-gerenciador-contador.component.spec.ts
+++ src/app/card-gerenciador-contador/card-gerenciador-contador.component.spec.ts
@@ -1,14 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Store } from '@ngrx/store';
 
 import { CardGerenciadorContadorComponent } from './card-gerenciador-contador.component';
 
 describe('CardGerenciadorContadorComponent', () => {
+  const storeSpy = jasmine.createSpyObj('Store', ['select']);
   let component: CardGerenciadorContadorComponent;
   let fixture: ComponentFixture<CardGerenciadorContadorComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CardGerenciadorContadorComponent ]
+      declarations: [ CardGerenciadorContadorComponent ],
+      providers: [{ provide: Store, useValue: storeSpy }]
     })
     .compileComponents();
   });
